f02444c61b7d4bb67f970e333edca8b5ef0db27c
Han Zhou <han>
2024-04-02 01:48:29 +0800

    第一次提交

---
+++ src/components/header.vue
@@ -0,0 +1,209 @@
+<!-- eslint-disable prettier/prettier -->
+<template>
+  <div class="header" :class="[{ fixedToTop }, themeColor]">
+    <div class="logo">
+      <a href="/">
+        <!-- <logo :is-transparent="themeColor === 'is-transparent'"></logo> -->
+        <img src="../assets/logo.png" style="width: 100px;"/>
+      </a>
+    </div>
+    <ul class="navbar">
+      <router-link to="/" exact v-slot="{ href, navigate, isActive }">
+        <li class="navbar-item" :class="{ active: isActive }">
+          <a :href="href" @click="navigate">主页</a>
+        </li>
+      </router-link>
+      <router-link exact to="/jobs" v-slot="{ href, navigate, isActive }">
+        <li class="navbar-item" :class="{ active: isActive }">
+          <a :href="href" @click="navigate">职位</a>
+        </li>
+      </router-link>
+    </ul>
+    <div class="user">
+      <div class="login" v-if="!state.isLogin">
+        <router-link to="/user">
+          <span class="login__text">登录</span>
+        </router-link>
+      </div>
+
+      <div class="dropdown-menu" v-else>
+        <span class="dropdown-menu__email">
+          {{ state.userInfo.email }}
+          <i class="arrow"></i>
+        </span>
+        <ul class="dropdown-menu__wrapper">
+          <li class="dropdown-menu__item">
+            <router-link to="/resume">我的简历</router-link>
+          </li>
+          <li class="dropdown-menu__item" @click="handleLogout">退出</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import store from "@/store";
+export default {
+  name: "Header",
+  data: () => ({
+    state: store.state
+  }),
+  created() {
+    store
+      .requestLoginStatus()
+      .then(isLogin => {
+        if (!isLogin) {
+          store.expireLogin();
+        } else if (isLogin && !this.state.userInfo.email) {
+          store
+            .requestUserInfo()
+            .then(() => {})
+            .catch(() => {});
+        }
+      })
+      .catch(() => {
+        store.expireLogin();
+      });
+  },
+  props: {
+    fixedToTop: {
+      type: Boolean,
+      default: false
+    },
+    themeColor: {
+      type: String,
+      default: "main-color"
+    }
+  },
+  methods: {
+    handleLogout() {
+      store
+        .requestLogout()
+        .then(() => {
+          this.$router.push("/");
+        })
+        .catch(err => {
+          throw err;
+        });
+    }
+  }
+};
+</script>
+<style lang="less" scoped>
+.header {
+  color: #aaa;
+
+  display: flex;
+  height: 60px;
+  align-items: center;
+  padding: 10px 100px;
+  min-width: 900px;
+  &.fixedToTop {
+    position: fixed;
+
+    width: 100%;
+  }
+  &.main-color {
+    background-color: rgba(254, 254, 254, 0.8);
+    .navbar-item {
+      &:hover {
+        color: @main-color;
+      }
+    }
+    .login-text {
+      color: @main-color;
+    }
+  }
+  &.is-transparent {
+    color: #fff;
+    .navbar-item {
+      &:hover {
+        color: #ccc;
+      }
+    }
+    .active {
+      color: #fff;
+    }
+  }
+}
+
+.logo {
+  width: 200px;
+  height: 100%;
+  a {
+    height: 100%;
+    display: block;
+  }
+}
+.navbar {
+  margin-left: auto;
+  margin-right: 20px;
+
+  display: flex;
+  &-item {
+    padding: 4px 0px;
+    margin: 0 20px;
+    &.active {
+      border-bottom: 2px solid;
+      color: @main-color;
+    }
+  }
+}
+.user {
+  margin-left: 40px;
+  position: relative;
+
+  .arrow {
+    display: inline-block;
+    border: 1px solid;
+    border-width: 1px 1px 0 0;
+    transform: rotate(135deg);
+    transform-origin: center;
+    vertical-align: 5px;
+    margin-left: 5px;
+    transition: all 0.3s;
+    width: 10px;
+    height: 10px;
+  }
+  &:hover {
+    .arrow {
+      transform: rotate(-45deg);
+      vertical-align: -3px;
+    }
+    .dropdown-menu__wrapper {
+      display: block;
+    }
+  }
+  .dropdown-menu {
+    position: relative;
+    z-index: 1000;
+    &__email {
+      line-height: 2;
+      cursor: pointer;
+    }
+    &__wrapper {
+      position: absolute;
+      display: none;
+      right: 0;
+      color: @regular-text-color;
+      padding: 9px 0;
+      width: 200px;
+      border-radius: 5px;
+      box-shadow: 0 0px 2px 1px #eee;
+      background: #fff;
+    }
+    &__item {
+      padding: 9px 12px;
+      cursor: pointer;
+      &:hover {
+        background: #efefef58;
+      }
+    }
+  }
+}
+.github-project {
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,466 @@
+<template>
+  <div class="home">
+    <div ref="banner" class="banner content-item-block">
+      <div class="video-wrapper">
+        <video
+          ref="video"
+          class="video"
+          preload="auto"
+          muted
+          loop
+          playsinline
+          autoplay
+          poster="../assets/bg.png"
+          src="../assets/bg.mp4"
+          tabindex="-1"
+        ></video>
+      </div>
+      <h1 class="banner-title">
+        和优秀的人 做有挑战的事
+        <div class="subTitle">JOIN BYTEDANCE</div>
+      </h1>
+      <div class="search">
+        <input-search
+          @search="searchJobs"
+          placeholder="输入城市或职位进行搜索"
+        ></input-search>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "home",
+  data() {
+    return {
+      search: ""
+    };
+  },
+  created() {},
+  methods: {
+    searchJobs(keyword) {
+      this.$router.push({ name: "jobs", params: { keyword } });
+    }
+  }
+};
+</script>
+
+<style lang="less" scoped>
+@keyframes verticalMotion {
+  0% {
+    transform: translateY(6px);
+  }
+  50% {
+    transform: translateY(-6px);
+  }
+  100% {
+    transform: translateY(6px);
+  }
+}
+@keyframes scrollToRight {
+  0% {
+    left: -100%;
+  }
+
+  100% {
+    left: 100%;
+  }
+}
+
+.home {
+  .main {
+    width: @main-width;
+    margin: auto;
+  }
+  .content-item-block {
+    .title {
+      font-size: @font-size-larger;
+    }
+  }
+}
+
+.banner {
+  height: 100vh;
+  min-height: 400px;
+  min-width: @main-width;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: rgba(51, 112, 255, 0.2);
+  position: relative;
+  .video-wrapper {
+    font-size: 0;
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    z-index: -1;
+    overflow: hidden;
+    .video {
+      object-fit: cover;
+      width: 100%;
+      height: 100%;
+    }
+  }
+  &-title {
+    color: #fff;
+    font-size: 60px;
+    text-align: center;
+    .subTitle {
+      &::after,
+      &::before {
+        content: "";
+        position: absolute;
+        background-image: linear-gradient(
+          270deg,
+          hsla(0, 0%, 100%, 0),
+          #fff 47%,
+          hsla(0, 0%, 100%, 0)
+        );
+        height: 2px;
+        width: 89px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      &:before {
+        right: 10px;
+      }
+      &:after {
+        left: 10px;
+      }
+      position: relative;
+      margin-top: 10px;
+      font-size: @font-size-base;
+      letter-spacing: 20px;
+    }
+  }
+  .search {
+    width: 500px;
+    margin-top: 100px;
+    margin-bottom: 40px;
+  }
+  .bottom-tips {
+    position: absolute;
+    bottom: 30px;
+    width: 50px;
+    height: 50px;
+    flex-shrink: 0;
+
+    text-align: center;
+    .arrow-down {
+      display: block;
+      margin-bottom: 4px;
+      width: 100%;
+      height: 100%;
+      border-radius: 50%;
+      border: 1px solid #fff;
+      text-align: center;
+      line-height: 40px;
+      color: #fff;
+      cursor: pointer;
+      font-size: 21px;
+      i {
+        animation: verticalMotion 1.3s infinite linear;
+        display: inline-block;
+        font-style: normal;
+      }
+    }
+    // 分割竖线
+    .vertical-dashed {
+      border-left: 1px dashed #fff;
+      height: 30px;
+
+      display: inline-block;
+    }
+  }
+}
+
+// 产品
+.product {
+  margin-top: 40px;
+  text-align: center;
+  padding: 40px 40px;
+  background: url(//sf1-ttcdn-tos.pstatp.com/obj/ttfe/ATSX/mainland/global.png);
+  background-size: 100%;
+  background-repeat: no-repeat;
+  background-position: 0 370px;
+  .desc {
+    margin: 40px 0;
+  }
+  &-list {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+  &-item {
+    width: 90px;
+    // height: 90px;
+    cursor: pointer;
+    &:nth-child(1),
+    &:nth-child(9) {
+      margin-top: 116px;
+    }
+    &:nth-child(2),
+    &:nth-child(8) {
+      margin-top: 80px;
+    }
+    &:nth-child(3),
+    &:nth-child(7) {
+      margin-top: 55px;
+    }
+    &:nth-child(4),
+    &:nth-child(6) {
+      margin-top: 30px;
+    }
+    &:hover {
+      color: @main-color;
+      img {
+        border-radius: 19px;
+
+        transform: scale(1.2);
+        transform-origin: bottom;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+      }
+    }
+    img {
+      width: 90px;
+      height: 90px;
+      border-radius: 19px;
+
+      margin-bottom: 10px;
+      transition: transform 0.3s;
+    }
+  }
+  .more {
+    margin: auto;
+    margin-top: 170px;
+
+    width: 149px;
+  }
+}
+
+// 职位
+
+.job-category {
+  text-align: center;
+  margin-top: 90px;
+
+  &-list {
+    margin-top: 70px;
+  }
+  &-item {
+    float: left;
+    margin-bottom: 16px;
+    .image {
+      width: 250px;
+      height: 250px;
+      line-height: 250px;
+      background-repeat: no-repeat;
+      background-size: 100%;
+      border-radius: 50%;
+      margin: 0 auto;
+
+      transition: all 0.3s;
+      background-position: center;
+      overflow: hidden;
+      &:hover {
+        box-shadow: 0 0 8px 1px #ccc;
+        background-size: 110%;
+        & + .name {
+          color: @main-color;
+        }
+      }
+      .overlay {
+        display: inline-block;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(0, 0, 0, 0.5);
+        color: #fff;
+
+        font-size: 40px;
+      }
+    }
+    .name {
+      margin-top: 23px;
+      font-size: @font-size-large;
+    }
+    margin: 0 2.5%;
+    margin-bottom: 30px;
+    width: 20%;
+  }
+}
+
+// 字节范
+
+.byteStandard {
+  padding: 40px 40px;
+  overflow: hidden;
+  .title {
+    margin-bottom: 30px;
+  }
+  .desc {
+    margin-bottom: 40px;
+  }
+  .content {
+    .image {
+      .layer {
+        // left: -100%;
+        width: 100%;
+        top: 0;
+        bottom: 0;
+        background: rgba(255, 255, 255, 0.8);
+        position: absolute;
+        animation: scrollToRight 0.8s ease-in-out;
+      }
+
+      position: relative;
+
+      float: left;
+      width: 700px;
+      height: 450px;
+      margin-right: 100px;
+      overflow: hidden;
+      img {
+        border-radius: 6px;
+        object-fit: cover;
+      }
+    }
+  }
+  .indicator {
+    color: #aaa;
+
+    &-item {
+      margin: 32px 0;
+      h3 {
+        &.active {
+          color: black;
+          font-size: 32px;
+          font-weight: 400;
+        }
+        cursor: pointer;
+      }
+    }
+  }
+}
+// 字节生活
+.byteLife {
+  // width:1200px;
+  // overflow: hidden;
+  &-title {
+    margin-bottom: 70px;
+  }
+  padding-left: 10%;
+  .block-item {
+    overflow: hidden;
+    margin-bottom: 100px;
+    &:nth-child(odd) {
+      .block-item-column {
+        &-text {
+          margin-right: 9%;
+        }
+        float: left;
+      }
+    }
+    &:nth-child(even) {
+      .block-item-column {
+        &-image {
+          margin-right: 9%;
+        }
+        float: right;
+      }
+    }
+
+    &-column {
+      &-title {
+        margin-bottom: 40px;
+        padding-bottom: 40px;
+        border-bottom: 1px solid #ccc;
+      }
+      &-text {
+        width: 30%;
+      }
+      &-image {
+        height: 400px;
+        width: 59%;
+        border-radius: 8px;
+        overflow: hidden;
+        img {
+          height: 100%;
+          object-fit: cover;
+        }
+      }
+    }
+  }
+}
+// 员工故事
+.staffStory {
+  padding: 0 140px;
+  .title {
+    text-align: center;
+    margin-bottom: 10px;
+  }
+
+  &-list {
+    text-align: center;
+    display: flex;
+    justify-content: space-between;
+    margin-top: 30px;
+  }
+  &-item {
+    // height: 450px;
+    overflow: hidden;
+    padding: 30px;
+    width: 30%;
+    cursor: pointer;
+
+    &:hover {
+      box-shadow: 0 10px 30px 0 @box-shadow-hover-color;
+      .circular-button {
+        color: @main-color;
+      }
+    }
+    .avatar {
+      width: 70%;
+      margin: auto;
+    }
+    .text {
+      .title {
+        color: @primary-text-color;
+        font-weight: @font-weight-regular;
+        font-size: @font-size-large;
+        margin: 10px 0;
+        white-space: nowrap;
+      }
+      .desc {
+        color: @primary-text-color;
+        margin: 12px 0;
+        font-size: @font-size-base;
+      }
+      .remark {
+        font-size: @font-size-base;
+
+        color: @regular-text-color;
+        -webkit-box-orient: vertical;
+        display: -webkit-box;
+        -webkit-line-clamp: 3;
+        overflow: hidden;
+        text-overflow: ellipsis;
+      }
+    }
+    .circular-button {
+      border-radius: 50%;
+      width: 30px;
+      height: 30px;
+      border: 1px solid;
+      line-height: 30px;
+
+      margin: auto;
+      margin-top: 20px;
+      color: #ccc;
+    }
+  }
+}
+</style>
